97b4dd48e8a9a4e8aa5ad82a7d8250e513840666
Thanhphu-95 <thanhphu1505@gmail.com>
2025-09-06 00:16:55 +0700

    tele

--- Assets/Scripts/Teleporer.cs
+++ Assets/Scripts/Teleporer.cs
@@ -1,12 +1,20 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Teleporer : MonoBehaviour
 {
     [SerializeField] private Transform destination;
+    [SerializeField] private CinemachineCamera MainMap;
+    [SerializeField] private CinemachineCamera teleMap;
 
     public Transform GetDestination()
     {
+        MainMap.gameObject.SetActive(false);
+        teleMap.gameObject.SetActive(true);
+
         return destination;
         Destroy(gameObject);
     }
+
+
 }
